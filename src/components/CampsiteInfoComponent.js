e98a1a3ed5574a7d19bb620fcbc4c36a3415b376
Joe Moreno <joe.moreno74@gmail.com>
2021-01-13 14:45:36 +0100

    Fetch Handling Errors

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -144,7 +145,7 @@ function ShowModal(props) {
             return (
             <div className="col-md-5 m-1">
                     <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />  
                         <CardBody>
                             <CardText>{campsite.description}</CardText>
                         </CardBody>
